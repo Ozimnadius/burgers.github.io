edc02d134b87ee46696c0effb3683a688276996e
Seregin Mikle <mikhail-seregin@yandex.ru>
2018-09-26 13:12:13 +0300

    js acco, hacco

--- js/main.js
+++ js/main.js
@@ -1,23 +1,57 @@
 $(function () {
-    $('.team__name').on('click', function (e) {
-        var button = $(this),
-            item = button.closest('.team__item'),
-            wrapperHeight = item.find('.team__wrapper').outerHeight(),
-            hidden = item.find('.team__hidden'),
-            items = $('.team__item').not(item),
-            hiddens = $('.team__hidden').not(hidden);
 
-        items.removeClass('active');
-        hiddens.css({
-            height: 0
+    const accoButtons = document.querySelectorAll('.team__name');
+    const accoItems = document.querySelectorAll('.team__item');
+    const accoHiddens = document.querySelectorAll('.team__hidden');
+
+    for (let i = 0; i < accoButtons.length; ++i) {
+        let button = accoButtons[i];
+        button.addEventListener('click', function () {
+            let item = this.parentElement,
+                wrapper = item.querySelector('.team__wrapper'),
+                wrapperStyles = getComputedStyle(wrapper),
+                wrapperHeight = parseInt(wrapperStyles.height),
+                hidden = item.querySelector('.team__hidden');
+
+
+            if (item.classList.contains('active')) {
+                hidden.style.height = 0;
+                item.classList.remove('active');
+
+            } else {
+                for (let j = 0; j < accoItems.length; ++j) {
+                    accoItems[j].classList.remove('active');
+                }
+                for (let j = 0; j < accoHiddens.length; ++j) {
+                    accoHiddens[j].style.height = 0;
+                }
+                hidden.style.height = wrapperHeight + 'px';
+                item.classList.add('active');
+            }
+
         });
-        hidden.css({
-            height: wrapperHeight
+    }
+
+
+    const haccoButtons = document.querySelectorAll('.haccor__title');
+    const haccoItems = document.querySelectorAll('.haccor__item');
+
+    for (let i = 0; i < haccoButtons.length; ++i) {
+        let button = haccoButtons[i];
+        button.addEventListener('click', function () {
+            let item = this.parentElement;
+
+            if (item.classList.contains('active')) {
+                item.classList.remove('active');
+            } else {
+                for (let j = 0; j < haccoItems.length; ++j) {
+                    haccoItems[j].classList.remove('active');
+                }
+                item.classList.add('active');
+            }
+
         });
-        item.addClass('active');
-
-
-    });
+    }
 
     const hamOpen = document.querySelector('.menu-button ');
     const hamClose = document.querySelector('.ham__close');
